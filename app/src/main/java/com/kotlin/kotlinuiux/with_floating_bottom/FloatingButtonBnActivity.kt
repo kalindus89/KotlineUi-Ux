6f9c4ad07707f15aba48a143e98dcd7d3bc31ebe
kalin <kalindusupun@gmail.com>
2022-02-24 13:24:39 +0530

    Kotlin Ui-Ux

---
+++ app/src/main/java/com/kotlin/kotlinuiux/with_floating_bottom/FloatingButtonBnActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.kotlinuiux.with_floating_bottom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.kotlinuiux.R
+
+class FloatingButtonBnActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_floating_button_bn)
+    }
+}

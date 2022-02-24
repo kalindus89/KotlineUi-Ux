55e37524c82cb1e173419f687e1a6a24de410bbe
kalin <kalindusupun@gmail.com>
2022-02-24 10:22:02 +0530

    Kotlin Ui-Ux

---
+++ app/src/main/java/com/kotlin/kotlinuiux/with_navigation_components/FragmentTwoNc.kt
@@ -0,0 +1,27 @@
+package com.kotlin.kotlinuiux.with_navigation_components
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kotlin.kotlinuiux.R
+
+
+class FragmentTwoNc : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two_nc, container, false)
+    }
+
+
+}

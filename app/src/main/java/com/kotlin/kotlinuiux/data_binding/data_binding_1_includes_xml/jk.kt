54639c9b9feaec8b5a7c69db0204ce121e3dd33c
kalin <kalindusupun@gmail.com>
2022-03-08 15:59:16 +0530

    Kotlin Ui-Ux

--- app/src/main/java/com/kotlin/kotlinuiux/data_binding/data_binding_1_includes_xml/jk.kt
+++
@@ -1,4 +0,0 @@
-package com.kotlin.kotlinuiux.data_binding.data_binding_1_includes_xml
-
-class jk {
-}
---
+++ app/src/main/java/com/kotlin/kotlinuiux/data_binding/data_binding_2way_1way/TwowayOneWayMMVMActivity.kt
@@ -0,0 +1,16 @@
+package com.kotlin.kotlinuiux.data_binding.data_binding_2way_1way
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.Bindable
+import com.kotlin.kotlinuiux.R
+
+class TwowayOneWayMMVMActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twoway_one_way_mmvmactivity)
+
+
+
+    }
+}
